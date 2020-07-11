87d2c53988b5226834a2e4635690181d4c7beb7a
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2020-07-11 18:12:17 +1000

    Add camera movement script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float movementSpeed = 0.005f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x += movementSpeed;
+        transform.position = newPosition;
+    }
+}
