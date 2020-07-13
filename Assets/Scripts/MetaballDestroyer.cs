960777307e420735a8d012de794a3feb6952f1bc
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2020-07-13 13:01:22 +1000

    Add metaball destruction and losing system

---
+++ Assets/Scripts/MetaballDestroyer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetaballDestroyer : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collider = collision.gameObject;
+        if (collider.layer == 8)
+        {
+            Destroy(collider);
+        }
+    }
+}
