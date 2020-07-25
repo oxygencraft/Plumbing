246d73efc00f9c43319a8189fac75076160f58a4
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2020-07-25 10:05:19 +1000

    Add obstacles system
    This commit will not add obstacle generation

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer != 8)
+            return;
+        FindObjectOfType<MetaballManager>().AddInfluencingAbility();
+        Destroy(collision.gameObject);
+    }
+}
