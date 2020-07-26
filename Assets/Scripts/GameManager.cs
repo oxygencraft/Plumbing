a2e8cef8c62542ef3081781e2792b5380ff85965
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2020-07-26 16:46:06 +1000

    JAM VERSION!!!!
    Add quit menu (I think) I don't actually know what happened this commit.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,9 +29,9 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void EndGame()
+    public void QuitGame()
     {
-
+        Application.Quit();
     }
 
     public void RestartGame()
